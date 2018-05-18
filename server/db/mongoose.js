76b46320dbe2b020ca8b5137690e87fe6fd67b11
Cristi Mitran <cristimitran@gmail.com>
2018-05-18 11:29:18 +0300

    changed db connections

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,14 @@
 var mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://cristi:test@ds227740.mlab.com:27740/heroku-test')
+
+if (process.env.MONGODB_URI) {
+    mongoose.connect('mongodb://cristi:test@ds227740.mlab.com:27740/heroku-test')
+} else {
+    mongoose.connect('mongodb://localhost:27017/TodoApp')
+}
+
+//mongoose.connect('mongodb://cristi:test@ds227740.mlab.com:27740/heroku-test')
 
 module.exports = {
     mongoose

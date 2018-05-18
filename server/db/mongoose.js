5996b951dbeed0c4d709ee858073e183253dd162
Cristi Mitran <cristimitran@gmail.com>
2018-05-18 11:44:16 +0300

    db config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-if (process.env.MONGODB_URI) {
+if (process.env.NODE_ENV) {
     mongoose.connect('mongodb://cristi:test@ds227740.mlab.com:27740/heroku-test')
 } else {
     mongoose.connect('mongodb://localhost:27017/TodoApp')

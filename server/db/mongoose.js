4088689c940efff00976929b66e8236a8b382d09
Cristi Mitran <cristimitran@gmail.com>
2018-05-22 17:46:10 +0300

    changed db paths

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,18 +3,6 @@ var config = require('./config.json')
 
 mongoose.Promise = global.Promise
 
-// if (process.env.NODE_ENV === 'production') {
-//     mongoose.connect('mongodb://cristi:test@ds227740.mlab.com:27740/heroku-test')
-// } else if (process.env.NODE_ENV === 'development') {
-//     process.env.PORT = 3000
-//     mongoose.connect('mongodb://localhost:27017/TodoApp')
-// } else if (process.env.NODE_ENV === 'test') {
-//     process.env.PORT = 3000
-//     mongoose.connect('mongodb://localhost:27017/TodoAppTest')
-// }
-
-//mongoose.connect('mongodb://cristi:test@ds227740.mlab.com:27740/heroku-test')
-
 var env = process.env.NODE_ENV || 'development';
 console.log('env ****', env);
 
